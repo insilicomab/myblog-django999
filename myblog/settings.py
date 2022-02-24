604d1d45ce9c7a7316427a0f195a44d3926e12ef
insilicomab <54536339+insilicomab@users.noreply.github.com>
2022-02-24 23:27:34 +0900

    S3のための記述

--- myblog/settings.py
+++ myblog/settings.py
@@ -138,6 +138,11 @@ LOGIN_URL = 'blog:login'
 LOGIN_REDIRECT_URL = 'blog:index'
 
 # S3のための記述
+AWS_STORAGE_BUCKET_NAME = 'myblog-django-999' # AWSのユーザー名
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400', # 1日はそのキャッシュを使う
+}
 AWS_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
