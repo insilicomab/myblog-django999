cdc54ec4ce551965b754a2b5a0e5484ec1d416ca
insilicomab <54536339+insilicomab@users.noreply.github.com>
2022-02-24 23:31:56 +0900

    修正

--- myblog/settings.py
+++ myblog/settings.py
@@ -137,19 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'blog:login'
 LOGIN_REDIRECT_URL = 'blog:index'
 
-# S3のための記述
-AWS_STORAGE_BUCKET_NAME = 'myblog-django-999' # AWSのバケット名
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400', # 1日はそのキャッシュを使う
-}
-AWS_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 # 画像の保存先
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
-MEDIA_URL = 'https://%s/%s' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+MEDIA_URL = '/media/'
 
 # herokuデプロイのための追記
 
@@ -175,3 +165,12 @@ if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    # S3のための記述
+    AWS_STORAGE_BUCKET_NAME = 'myblog-django-999' # AWSのバケット名
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400', # 1日はそのキャッシュを使う
+    }
+    AWS_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = 'https://%s/%s' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
